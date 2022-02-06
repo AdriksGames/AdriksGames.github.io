18fb4a0b766b9e339df105ba0301f683cbb1764a
AdriksGames <adriksgames.pl@gmail.com>
2022-02-06 17:42:28 +0100

    Init

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Games from "../views/games/Games.vue";
+import GameDetails from "../views/games/GameDetails.vue";
+import NotFound from "../views/NotFound.vue";
 
 const routes = [
   {
@@ -8,13 +11,20 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/games",
+    name: "Games",
+    component: Games,
+  },
+  {
+    path: "/games/:id",
+    name: "GameDetails",
+    component: GameDetails,
+    props: true,
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
   },
 ];
 
